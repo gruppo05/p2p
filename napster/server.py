86cc56e396cd37308880a80003ba6fa18ee9e535
Francesco <francesco.bertasi@student.unife.it>
2018-02-27 18:19:30 +0100

    server timidamente funzionante

--- napster/server.py
+++ napster/server.py
@@ -1,16 +1,37 @@
 import socket
-TCP_IP = '172.16.5.1'
-TCP_PORT = 3000
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-	data = conn.recv(BUFFER_SIZE)
-	if not data: break
-	print "received data:", data
-	conn.send(data)
-conn.close()
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    
+    
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print >>sys.stderr, 'received "%s"' % data
+            if data:
+                print >>sys.stderr, 'sending data back to the client'
+                connection.sendall(data)
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+            
+    finally:
+        # Clean up the connection
+        connection.close()
