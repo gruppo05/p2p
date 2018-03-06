816f67df0896e205197dff951dc334cfd7e2bfc7
dek <ddario.decarlo@gmail.com>
2018-03-06 18:19:53 +0100

    parte dek ADDF aggiunta ma non funzionante

--- napster/napster.py
+++ napster/napster.py
@@ -7,6 +7,17 @@ from random import *
 def sessionIdGenerator():
 	return "".join(choice(string.ascii_letters + string.digits) for x in range(16))
 
+def setCopy(getCopy):
+	copy = int(getCopy[0])
+	print(copy)
+	if copy > 1000:
+		copy = 999
+	elif copy < 100 and copy > 9:
+		copy = "0"+str(copy)
+	elif copy < 10:
+		copy = "00"+str(copy)
+	return copy
+
 class Napster(object):
 	def __init__(self):
 		IP = "127.0.0.1"
@@ -16,12 +27,13 @@ class Napster(object):
 		conn = sqlite3.connect(':memory:')
 		print("Creato db", conn)
 		self.dbReader = conn.cursor()
-
+		
 		# Creo tabella user
 		self.dbReader.execute("DROP TABLE IF EXISTS user")
-		self.dbReader.execute('''CREATE TABLE user (SessionID text, IPP2P text, PP2P text)''')
-		print("Tabella user creata...")
-
+		self.dbReader.execute("CREATE TABLE user (SessionID text, IPP2P text, PP2P text)")
+		self.dbReader.execute("CREATE TABLE file (Filemd5 text, filename text, SessionID text)")
+		print("Tabelle user, file create...")
+		
 		# Creo socket
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.server_address = (IP, PORT)
@@ -63,6 +75,21 @@ class Napster(object):
 					print("Find file name")
 				elif command == "ADDF":
 					print("Add file")
+					SessionID = connection.recv(16).decode()
+					Filemd5 = connection.recv(32).decode()
+					Filename = connection.recv(100).decode()
+					
+					self.dbReader.execute("SELECT SessionID from file where Filemd5=?",(Filemd5,))
+					data = self.dbReader.fetchone()
+					if data is None:
+						self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",(Filemd5, Filename, SessionID))
+					else:
+						self.dbReader.execute("UPDATE file SET Filename=?",(Filename,))
+					self.dbReader.execute("SELECT COUNT(Filemd5) from file where Filemd5=?",(Filemd5,))
+					copy = self.dbReader.fetchone()
+					copy = setCopy(copy)
+					connection.sendall(("AADD"+copy).encode())
+					
 				elif command == "LOGO":
 					print("Log out")
 				elif command == "DREG":
@@ -72,6 +99,13 @@ class Napster(object):
 				elif command == "STAM":
 					#Stampo
 					self.dbReader.execute("SELECT * FROM user")
+					for row in self.dbReader:
+						print(row)
+				# *************************************************
+				# *************** DA TOGLIERE *********************
+				elif command == "DEKK":
+					#Stampo
+					self.dbReader.execute("SELECT * FROM file")
 					for row in self.dbReader:
 						print(row)
 				# *************************************************
