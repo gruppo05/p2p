7ad53665d99b15bda7426a8e59626c5976f73388
Francesco <francesco.bertasi@student.unife.it>
2018-03-06 14:43:28 +0100

    implementazione per python3 non funzionante

--- napster/client.py
+++ napster/client.py
@@ -3,14 +3,15 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server_address = (sys.argv[1], 10000)
-server_address = ("127.0.0.1", 3000)
-print('connecting to %s port %s', server_address)
+server_address = ('127.0.0.1', 3000)
+print("starting up on port", server_address)
 sock.connect(server_address)
 
 try:
 	message = sys.argv[1]
-	print "Inviato: ", message
+	print("Inviato:", message)
 	sock.sendall(message)
 	
 finally:
     sock.close()
+
--- napster/napster.py
+++ napster/napster.py
@@ -12,63 +12,60 @@ class Napster(object):
 		
 		# Creo DB
 		conn = sqlite3.connect(':memory:')
-		print "Creato db %s" % conn
+		print("Creato db", conn)
 		c = conn.cursor()
 		
 		# Creo tabella user
 		c.execute("DROP TABLE IF EXISTS user")
 		c.execute('''CREATE TABLE user (SessionID text, IPP2P text, PP2P text)''')
-		print "Tabella user creata..."
+		print("Tabella user creata...")
 		
 		# Creo socket
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server_address = (IP, PORT)
 		sock.bind(server_address)
 		
-		print "Server attivo su %s porta %s" % server_address
+		print("Server attivo su porta", server_address)
 		sock.listen(1)
 		while True:
-			print "In attesa di connessione..."
+			print("In attesa di connessione...")
 			connection, client_address = sock.accept()
 			
 			try:
-				print "Connessione da", client_address
+				print("Connessione da", client_address)
 				command = str(connection.recv(4))
-				print "Ricevuto", command
+				print("Ricevuto", command)
 				if command == "LOGI":
-					
 					IPP2P = str(connection.recv(55))
 					IPP = str(connection.recv(5))
 			
 					#Cerco IP utente
 					result = c.execute("SELECT SessionID FROM user WHERE IPP2P=?", (IPP2P,))
- 					if result.fetchone() is None:
- 						print "*************** NON TROVATO ***************"
- 						SessionID = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(16)])
- 				
- 						#Inserimento
+					if result.fetchone() is None:
+						print("*************** NON TROVATO ***************")
+						SessionID = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(16)])
+
+						#Inserimento
 						c.execute("INSERT INTO user (SessionID, IPP2P, PP2P) values (?, ?, ?)",
-            			(SessionID, IPP2P, IPP))
-            	
- 					else:
- 						print "*************** TROVATO ***************"
- 						c.execute("SELECT SessionID FROM user WHERE IPP2P=?", (IPP2P,))
+						(SessionID, IPP2P, IPP))
+					else:
+						print("*************** TROVATO ***************")
+						c.execute("SELECT SessionID FROM user WHERE IPP2P=?", (IPP2P,))
 						data = c.fetchone() #retrieve the first row
 						SessionID = data[0]
-						
-					print "SessionID:", SessionID		
+					print("SessionID:", SessionID)
+					connection.sendall(SessionID)	
 					#Da implementare processio di invio del SessionID...
-						
 				if command == "DELF":
-					print "Delete file"
+					print("Delete file")
 				elif command == "FIND":
-					print "Find file name"
+					print("Find file name")
 				elif command == "ADDF":
-					print "Add file"
+					print("Add file")
 				elif command == "LOGO":
-					print "Log out"
+					print("Log out")
 				elif command == "DREG":
-					print "Download"
+					print("Download")
 					
 				# *************** DA TOGLIERE *********************
 				elif command == "STAM":
@@ -78,7 +75,7 @@ class Napster(object):
 						print(row)
 				# *************************************************
 			except:
-				print "Errore lato server"
+				print("Errore lato server")
 			finally:
 				connection.close()
 				
