0d64350b6d545eab0feffa275b39ca842324919e
Francesco <francesco.bertasi@student.unife.it>
2018-03-13 16:29:52 +0100

    versione 1.0 da testare con access point

--- napster/napster.py
+++ napster/napster.py
@@ -7,7 +7,7 @@ def clearAndSetDB(self):
 	self.dbReader.execute("DROP TABLE IF EXISTS download")
 	self.dbReader.execute("CREATE TABLE user (SessionID text, IPP2P text, PP2P text)")
 	self.dbReader.execute("CREATE TABLE file (Filemd5 text, Filename text, SessionID text)")
-	self.dbReader.execute("CREATE TABLE download (Filemd5 text, Download text)")
+	self.dbReader.execute("CREATE TABLE download (Filemd5 text, Download integer)")
 	print("Tabelle user, file create...")
 	
 def sessionIdGenerator():
@@ -15,7 +15,6 @@ def sessionIdGenerator():
 
 def setCopy(getCopy):
 	copy = int(getCopy[0])
-	print(copy)
 	if copy > 1000:
 		copy = 999
 	elif copy < 100 and copy > 9:
@@ -24,10 +23,23 @@ def setCopy(getCopy):
 		copy = "00"+str(copy)
 	return copy
 
+def setDownload(getDownload):
+	download = int(getDownload[0])
+	if download > 100000:
+		download = 99999
+	elif download < 10000 and download > 999:
+		download = "0"+str(download)
+	elif download < 1000 and download > 99:
+		download = "00"+str(download)
+	elif download < 100 and download > 9:
+		download = "000"+str(download)
+	elif download < 10:
+		download = "0000"+str(download)
+	return download
+
 class Napster(object):
 	def __init__(self):
-		IPv4 = "127.0.0.1"
-		IPv6 = "0000:0000:0000:0000:0000:0000:0000:0001"
+		IP = ''
 		PORT = 3000
 		
 		# Creo DB
@@ -38,27 +50,33 @@ class Napster(object):
 		# Creo tabella user
 		clearAndSetDB(self)
 		
-		# Socket ipv4
-		self.server_address = (IPv4, PORT)
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#DA CANCELLARE
+		self.dbReader.execute("INSERT INTO user (SessionID, IPP2P, PP2P) values (?, ?, ?)",('cIEeEGMv7mLVLxfB', '244.255.255.289|ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff', 80))
+		self.dbReader.execute("INSERT INTO user (SessionID, IPP2P, PP2P) values (?, ?, ?)",(58876, 'SENDNUDES', 81))
+		self.dbReader.execute("INSERT INTO user (SessionID, IPP2P, PP2P) values (?, ?, ?)",(45017, 'Luca', 82))
+		self.dbReader.execute("INSERT INTO user (SessionID, IPP2P, PP2P) values (?, ?, ?)",(96859, '1211535435353sticazzi', 424))
+		
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('12345678912345671234567891234567', 'MarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMarioMario', 'cIEeEGMv7mLVLxfB'))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('primofile', 'Giorgio', 'cIEeEGMv7mLVLxfB'))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('726362762', 'Luca', 58876))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('primofile', 'Giorgio', 96859))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('123445789', 'Arabo', 45017))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('primofile', 'Giorgio', 54545))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('724662762', 'Maria', 58876))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('secondofile', 'Giorgia', 96859))
+		self.dbReader.execute("INSERT INTO file (Filemd5, Filename, SessionID) values (?, ?, ?)",('primofile', 'Giorgia', 58876))
+
+		# Socket ipv4/ipv6
+		self.server_address = (IP, PORT)
+		self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.sock.bind(self.server_address)
 		self.sock.listen(5)
-		
-		# Socket ipv6
-		self.server_address6 = (IPv6, PORT)
-		self.sock6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-		self.sock6.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.sock6.bind(self.server_address6)
-		self.sock6.listen(5)
 		
 	def start(self):
 		while True:
 			print("In attesa di connessione...")
 			connection, client_address = self.sock.accept()
-			print(client_address)
-			connection, client_address = self.sock6.accept()
-			print(client_address)
 			
 			try:
 				print("Connessione da", client_address)
@@ -103,36 +121,36 @@ class Napster(object):
 					connection.sendall(("ADEL"+str(copy)).encode())
 					print("Inviato numero di copie --> ",copy)
 					
-				#--------------------DA TESTARE-------------------------------------------------#	
 				elif command == "FIND":
 					print("Find file name")
+		
 					SessionID = connection.recv(16).decode()
 					Ricerca = connection.recv(20).decode()
 					#Conto quanti file con filemd5 diverso ci sono con filename simile a ricerca
-					self.dbReader.execute("SELECT count(*) FROM file WHERE Filename LIKE ? GROUP BY Filemd5", ('%' + Ricerca + '%',))
+					self.dbReader.execute("SELECT count(*) FROM (SELECT DISTINCT Filemd5 FROM file WHERE Filename LIKE ?)", ('%' + Ricerca + '%',))
 					idmd5 = self.dbReader.fetchone()
+		
 					#Prendo tutti i filemd5 singolarmente
 					self.dbReader.execute("SELECT DISTINCT Filemd5 FROM file WHERE Filename LIKE ?", ('%' + Ricerca + '%',))
 					resultFilemd5 = self.dbReader.fetchall()
-					
 					#Per ogni file ciclo per cercare chi ha questi file
 					idmd5 = setCopy(idmd5)
-					
+		
 					msg = "AFIN" + idmd5
 					for md5 in resultFilemd5:
 						self.dbReader.execute("SELECT Filename FROM file WHERE filemd5 = ?",(md5[0],))
 						resultFilename = self.dbReader.fetchone()
-						self.dbReader.execute("SELECT count(*) FROM file WHERE Filemd5=?", (Filemd5,))
+						self.dbReader.execute("SELECT count(*) FROM file WHERE Filemd5=?", (md5[0],))
+						print("md5 i-esimo= ", md5[0])
 						copy = self.dbReader.fetchone()
 						copy = setCopy(copy)
-						self.dbReader.execute("SELECT IPP2P, PP2P FROM user JOIN file WHERE user.SessionID = file.SessionID AND filemd5 = ?", (md5[0]))
+						self.dbReader.execute("SELECT IPP2P, PP2P FROM user JOIN file WHERE user.SessionID = file.SessionID AND filemd5 = ?", (md5[0],))
 						resultIP = self.dbReader.fetchall()
-						msg = msg + md5[0] + resultFilename + copy
+						msg = msg + md5[0] + resultFilename[0] + copy
 						for ip in resultIP:
-							msg = msg + ip
+							msg = msg + ip[0] + ip[1]
 					
 					connection.sendall(msg.encode())
-				#---------------------------------------------------------------------------------#
 					
 				elif command == "ADDF":
 					print("Add file")
@@ -162,6 +180,7 @@ class Napster(object):
 					delete = self.dbReader.fetchone()
 					delete = setCopy(delete)
 					self.dbReader.execute("DELETE FROM file WHERE SessionID=?", (SessionID,))
+					self.dbReader.execute("DELETE FROM user WHERE SessionID=?", (SessionID,))
 					#magari controllo se è andato a buon fine?
 					connection.sendall(("ALGO"+delete).encode())
 					
@@ -169,11 +188,17 @@ class Napster(object):
 					print("Download")
 					SessionID = connection.recv(16).decode()
 					Filemd5 = connection.recv(32).decode()
-					self.dbReader.execute("SELECT COUNT(*) FROM file WHERE Filemd5 = ?",(Filemd5,))
-					idmd5=self.dbReader.fetchone()
-					idmd5 = setCopy(idmd5)
-					msg="ADRE"+idmd5
-					connection.sendall(msg.encode())
+					self.dbReader.execute("SELECT Download FROM download WHERE Filemd5 = ?",(Filemd5,))
+					download=self.dbReader.fetchone()
+					if download is None:
+						download = 1
+						self.dbReader.execute("INSERT INTO download (Filemd5, Download) values (?, ?)", (Filemd5, 1))
+					else:
+						download = download + 1
+						self.dbReader.execute("UPDATE download SET Download = ? WHERE Filemd5 = ?",(download, Filemd5,))
+					download = setDownload(download)
+					
+					connection.sendall(("ADRE"+download).encode())
 							
 				# *************** DA TOGLIERE *********************
 				
