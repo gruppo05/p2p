08d2c319fc7a7992ff83607e1f34c0ead04fb654
Francesco <francesco.bertasi@student.unife.it>
2018-04-06 14:08:03 +0200

    faccio pull

--- gnutella/serverGnutella.py
+++ gnutella/serverGnutella.py
@@ -41,14 +41,14 @@ class color:
 class GnutellaServer(object):
 	def __init__(self):
 
-		IP = "192.168.43.131"
+		IP = "192.168.43.135"
 		self.PORT = 4000
 
 		UDP_IP = "127.0.0.1"
 		UDP_PORT = 49999
 		
 		#MODIFICAMI CON IL TUO IP
-		self.myIPP2P = "192.168.043.131|0000:0000:0000:0000:0000:0000:0000:0001"
+		self.myIPP2P = "192.168.043.135|0000:0000:0000:0000:0000:0000:0000:0001"
 		self.myPort = 4000		
 
 		# Creo DB
