5bfc07ac5d9d3703d521ae2741f6883e42db433d
Francesco <francesco.bertasi@student.unife.it>
2018-05-07 10:23:41 +0200

    corretto gnutella con scelta download da testare

--- gnutella/settings.py
+++ gnutella/settings.py
@@ -1,8 +1,7 @@
 class Settings(object):
-	PORT = 3000
-	myIPP2P = "192.168.043.032|fe80:0000:0000:0000:b6bd:2104:d5cb:42b8"
-	
-	root_IP = "192.168.043.078|fe80:0000:0000:0000:b6bd:2104:d5cb:42b8"
-	root_PORT = "3000"
-	userPath = "/home/ale/p2p/image"
+	PORT = 50000
+	myIPP2P = "172.016.005.001|fc00:0000:0000:0000:0000:0000:0005:0001"
 
+	root_IP = "172.016.005.001|fc00:0000:0000:0000:0000:0000:0005:0001"
+	root_PORT = "50000"
+	userPath = "/home/francesco/Scrivania/Università/P2P/kazaa/images/"
