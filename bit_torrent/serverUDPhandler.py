e0bd6c2af04f169a99320bb6e6f758cb4f493dbc
Francesco <francesco.bertasi@student.unife.it>
2018-05-08 18:19:31 +0200

    big bang

---
+++ bit_torrent/serverUDPhandler.py
@@ -0,0 +1,210 @@
+import socket, sqlite3, string, subprocess, threading, os, random, ipaddress, time, datetime, hashlib, sys, stat
+import setting as var
+from threading import Timer
+from random import *
+
+class color:
+	HEADER = '\033[95m'
+	recv = '\033[36m'
+	green = '\033[32m'
+	send = '\033[33m'
+	fail = '\033[31m'
+	end = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+def clearAndSetDB(self):
+	self.dbReader.execute("DROP TABLE IF EXISTS File")
+	self.dbReader.execute("DROP TABLE IF EXISTS TrackedFile")
+
+	self.dbReader.execute("CREATE TABLE File (Filemd5 text, Filename text, SessionID text)")
+	self.dbReader.execute("CREATE TABLE TrackedFile (IPP2P text, PP2P text, Filemd5 text, Filename text)")
+    
+def setIp(n):
+	if n < 10:
+		n = "00"+str(n)
+	elif n < 100:
+		n = "0"+str(n)
+	return n
+	
+def setIPv6(n):
+	if n < 10:
+		n = "000"+str(n)
+	elif n < 100:
+		n = "00"+str(n)
+	elif n < 1000:
+		n = "0"+str(n)
+	return n	
+
+def splitIp(ip):
+	splitted = ip.split(".")
+	ip = str(int(splitted[0]))+"."+str(int(splitted[1]))+"."+str(int(splitted[2]))+"."+str(int(splitted[3]))
+	return ip
+	
+def setConnection(ip, port, msg):
+	try:
+		rnd = random()
+		if(rnd<0.5):
+			ip = splitIp(ip[0:15])						
+			print(color.green+"Connessione IPv4:"+ip+ " PORT:"+str(port)+color.end)
+			peer_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			peer_socket.connect((ip,port))
+
+		else:
+			ip = ip[16:55]
+			print(color.green+"Connetto con IPv6:"+ip+" PORT:"+str(port)+color.end);
+			peer_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+			peer_socket.connect((ip, port))
+
+		print("Invio --> "+color.send+msg+color.end)
+		peer_socket.sendall(msg.encode())
+	except:
+		print(color.fail+"Errore connessione 'not close'"+color.end)
+	return peer_socket
+	
+class bitTorrent(object):
+	def __init__(self):
+		self.ServerIP = ""
+		self.ServerPORT = 3000
+		
+		self.PORT = var.Settings.PORT
+		self.myIPP2P = var.Settings.myIPP2P
+		self.mySessionID = ""
+		
+		self.UDP_IP = "127.0.0.1"
+		UDP_PORT_SERVER = 49999
+		self.UDP_PORT_CLIENT = 50000
+	
+		self.timeDebug = var.Settings.timeDebug
+		self.BUFF = 1024
+		
+		# Creo DB
+		conn = sqlite3.connect(':memory:', check_same_thread=False)
+		self.dbReader = conn.cursor()
+		
+		# Creo tabella user
+		clearAndSetDB(self)
+		
+		# Socket ipv4/ipv6 port 3000
+		self.server_address = ("", int(self.PORT))
+		self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.sock.bind(self.server_address)
+		self.sock.listen(5)
+		
+		# socket upd ipv4 internal Server
+		self.sockUDPServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sockUDPServer.bind((self.UDP_IP, UDP_PORT_SERVER))
+		
+		# socket upd ipv4 client in uscita
+		self.sockUDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
+		
+	def server(self):
+		#crea thread interno per far comunicare client e server
+		threading.Thread(target = self.serverUDP, args = '').start()
+		
+		print(color.green+"In attesa di connessione esterna..."+color.end)
+		while True:
+			try:
+				connection, client_address = self.sock.accept()
+				threading.Thread(target = self.download, args = (connection,client_address)).start()
+			except:
+				return False
+	
+	def serverUDP(self):
+		print(color.green+"In attesa di comandi interni..."+color.end)
+		while True:
+			data, addr = self.sockUDPServer.recvfrom(4)
+			command = data.decode()
+			print("\n\nRicevuto comando dal client: "+color.recv+command+color.end)
+			
+				
+			elif command == "SETV":
+				gruppo = str(self.sockUDPServer.recvfrom(3)[0].decode())
+				numPc = str(self.sockUDPServer.recvfrom(3)[0].decode())
+				port = str(self.sockUDPServer.recvfrom(5)[0].decode())
+				ip = "172.016."+gruppo+"."+numPc
+				ipv6 = "fc00:0000:0000:0000:0000:0000:"+setIPv6(int(gruppo))+":"+setIPv6(int(numPc))
+				
+				#setto il server IP
+				self.ServerIP = ip+"|"+ipv6
+				self.ServerPORT = port
+		
+			elif command == "LOGI":
+				msg = "LOGI"+str(self.myIPP2P).ljust(55)+str(self.PORT).ljust(5)
+				peer_socket = setConnection(self.ServerIP, self.ServerPORT, msg)
+				command = peer_socket.recv(4).decode()
+				if command == "ALGI":
+					try:
+						#setto il mio sessionID
+						self.mySessionID = peer_socket.recv(16).decode()
+						self.sockUDPClient.sendto(("LOG1").encode(), (self.UDP_IP, self.UDP_PORT_CLIENT))
+					except:
+						print(color.fail+"Errore salvataggio SessionID"+color.end)
+						self.sockUDPClient.sendto(("LOG0").encode(), (self.UDP_IP, self.UDP_PORT_CLIENT))
+				else:
+					print(color.fail+"Login fallito!")
+					self.sockUDPClient.sendto(("LOG0").encode(), (self.UDP_IP, self.UDP_PORT_CLIENT))
+				peer_socket.close()
+			
+			elif command == "STOP":
+				print(color.fail+"Server fermato"+color.end)
+				self.sockUDPServer.close()
+				self.sockUDPClient.close()
+				os._exit(0) 
+
+	def download(self, connection, client_address):
+		command = connection.recv(4).decode()
+		try:
+			'''
+			elif command == "RETR":
+				print("Ricevuto "+color.recv+"RETR"+color.end)
+			
+				#inviare un file che ho
+				FileMD5 = connection.recv(32).decode()
+				print("FileMD5", FileMD5)
+				self.dbReader.execute("SELECT Filename FROM File WHERE FileMD5 = ?",(FileMD5,))
+				resultFile = self.dbReader.fetchone()
+				filename=resultFile[0].replace(" ","")
+			
+				try:
+					fd = os.open(var.Settings.userPath+""+filename, os.O_RDONLY)
+				except OSError as e:
+					print(e)
+				if fd is not -1:
+					filesize = int(os.path.getsize(var.Settings.userPath+""+filename))
+					num = int(filesize) / self.BUFF
+					if (filesize % self.BUFF)!= 0:
+						num = num + 1
+				
+					num = int(num)
+					msg = "ARET" + str(num).zfill(6)
+					
+					print ('Trasferimento iniziato di ', resultFile[0], ' [BYTES ', filesize, ']')
+					print(5)
+					#funzione progressBar
+					connection.send(msg.encode())
+					i = 0
+					while i < num:
+						buf = os.read(fd,self.BUFF)
+						
+						if not buf: break
+						lbuf = len(buf)
+						lbuf = str(lbuf).zfill(5)
+						connection.send(lbuf.encode())
+						connection.send(buf)
+						i = i + 1
+					
+					os.close(fd)
+					print(color.green+"\nFine UPLOAD"+color.end)					
+					connection.close()
+				else: 	
+		except:
+			connection.close()
+			return False
+			
+			'''
+
+if __name__ == "__main__":
+    bitTorrent = bitTorrent()
+bitTorrent.server()
