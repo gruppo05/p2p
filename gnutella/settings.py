94b438efdff23f26ef063020c6cf8cb988a36a0a
Francesco <francesco.bertasi@student.unife.it>
2018-05-08 10:06:46 +0200

    provo a pushà

--- gnutella/settings.py
+++ gnutella/settings.py
@@ -2,6 +2,6 @@ class Settings(object):
 	PORT = 50000
 	myIPP2P = "172.016.005.001|fc00:0000:0000:0000:0000:0000:0005:0001"
 	
-	root_IP = "172.016.005.001|fc00:0000:0000:0000:0000:0000:0005:0001"
+	root_IP = "172.016.005.003|fc00:0000:0000:0000:0000:0000:0005:0003"
 	root_PORT = "50000"
 	userPath = "/home/francesco/Scrivania/Università/P2P/kazaa/images/"
