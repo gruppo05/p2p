64909177634217d52284084f63fdf2c9db5ed879
Francesco <francesco.bertasi@student.unife.it>
2018-05-22 09:09:09 +0200

    merge con fazzi mattia

--- kazaa/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings(object):
-	PORT = 3000
-	myIPP2P = "192.168.001.010|fe80:0000:0000:0000:b6bd:2104:d5cb:42b8"
-	
-	root_IP = "192.168.001.001|fe80:0000:0000:0000:b6bd:2104:d5cb:42b8"
-	root_PORT = "3000"
-	userPath = "/home/P2P"
-
